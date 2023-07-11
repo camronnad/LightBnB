0fe83113c1782ea9dc78d6222de9c8d5befa4b38
Camron Naderi <camronnaderi57@gmail.com>
2023-07-10 20:29:59 -0700

    changed order of tables

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL
+  password VARCHAR(255) NOT NULl
 );
 
 CREATE TABLE properties (
@@ -32,13 +32,7 @@ CREATE TABLE properties (
   active BOOLEAN NOT NULL DEFAULT TRUE
 );
 
-CREATE TABLE reservations (
-  id SERIAL PRIMARY KEY NOT NULL,
-  start_date DATE NOT NULL,
-  end_date DATE NOT NULL,
-  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
-  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
-);
+
 
 CREATE TABLE property_reviews (
   id SERIAL PRIMARY KEY NOT NULL,
@@ -48,3 +42,14 @@ CREATE TABLE property_reviews (
   rating SMALLINT NOT NULL DEFAULT 0,
   message TEXT
 );
+
+
+CREATE TABLE reservations (
+  id SERIAL PRIMARY KEY NOT NULL,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
+
+
